1b2d5a8d1931d27e1b901e4fd62a3413df3faeef
Binit Tiwari <binit12tiwari@gmail.com>
2023-03-25 17:07:18 +0530

    react js website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex justify-between items-center max-w-7xl mx-auto'></div>
+  )
+}
+
+export default Navbar
